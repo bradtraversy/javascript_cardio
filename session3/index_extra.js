1865e44bb1fd0d23994ab77ba114ba7b279ca7de
Brad Traversy <traversymedia@gmail.com>
2018-10-25 19:37:55 -0400

    Changed directions

--- session3/index_extra.js
+++ session3/index_extra.js
@@ -10,7 +10,7 @@
 function addAll() {}
 
 // CHALLENGE 2: SUM ALL PRIMES
-// Pass in a number and loop through and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
 function sumAllPrimes() {}
