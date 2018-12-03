3350d154f4e0de50f389449dc3f8e36251a197c7
soufyakoub <41545690+soufyakoub@users.noreply.github.com>
2018-12-03 21:40:26 +0000

    Advanced Code Solution (most performant)
    the person who submitted this was given the same problem in an interview (spoiler: he wasn’t hired :(  ) he quickly arrived at the basic solution, and the interviewer told him to make it better. The algorithm would take way too long if he passed the Bible as the string. He wanted it to be instant.
    
    The simpler solutions perform very poorly on long strings because they operate on the whole string multiple times (toLowerCase(), replace(), split(), reverse(), join()) before comparing the whole string twice.
    
    The beauty of this solution is it never needs to read through the whole string, even once, to know that it’s not a palindrome. Why read through the whole string if you can tell that it’s not a palindrome just by looking at two letters?
    
    Uses a while loop instead of a for loop as a best practice - because we are using two variables, one is the index starting from the beginning of the string, and the other starts at the end of the string.

--- session1/index_extra.js
+++ session1/index_extra.js
@@ -42,6 +42,41 @@ function isPalindrome(str) {
   //         }
   //     }
   //     return isPali
+  
+  
+  
+  //  this solution performs at minimum 7x better, at maximum infinitely better.
+  //  read the explanation for the reason why. the guy that submitted this just failed it in an interview.
+    function palindrome(str) {
+      //assign a front and a back pointer
+      let front = 0
+      let back = str.length - 1
+
+      //back and front pointers won't always meet in the middle, so use (back > front)
+      while (back > front) {
+        //increments front pointer if current character is not a letter
+        if ( str[front].match(/[\W_]/) ) {
+          front++
+          continue
+        }
+        //decrements back pointer if current character  is not a letter
+        if ( str[back].match(/[\W_]/) ) {
+          back--
+          continue
+        }
+        //finally does the comparison on the current character
+        if ( str[front].toLowerCase() !== str[back].toLowerCase() ) return false
+        front++
+        back--
+      }
+
+      //if the whole string has been compared without returning false, it's a palindrome!
+      return true
+
+    }
+
+  
+  
 }
 
 // CHALLENGE 3: REVERSE AN INTEGER
