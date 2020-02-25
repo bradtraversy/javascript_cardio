30fe436c7946dc626154f79c19587c685227b52e
Brad Traversy <traversymedia@gmail.com>
2020-02-25 17:35:44 -0500

    Revert "Replit readme"

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,7 +3,7 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return str.split('').reverse().join('')
+  return 'Let\'s Start'
 }
 
 
@@ -12,11 +12,7 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {
-  const revStr = str.split('').reverse().join('');
-  return revStr === str
-  
- }
+function isPalindrome(str) {}
 
 
 
@@ -24,37 +20,31 @@ function isPalindrome(str) {
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) { 
-  const rev = int.toString().split('').reverse().join('');
-  return parseInt(rev) * Math.sign(int);
-}
+function reverseInt(int) {}
+
+
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) { }
+function capitalizeLetters(str) {}
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) { }
+function maxCharacter(str) {}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() { 
-  
-}
+function fizzBuzz() {}
 
 
 
 // Call Function
-// const output = reverseString('hello');
-// const output = isPalindrome('racecar');
-// const output = isPalindrome('hello');
-const output = reverseInt(-12345)
+const output = reverseString('hello');
 
 console.log(output);
