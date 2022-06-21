0b2a498c7802057754d308935d7b98092fc10116
kostandinos <82898197+KostandinosVas@users.noreply.github.com>
2022-06-21 13:02:14 +0300

    Update index_finished.js
    I just removed the -1 on reverseString function on the for loop and changed the >= 0 to > 0. I think is more elegant this way

--- session1/index_finished.js
+++ session1/index_finished.js
@@ -12,7 +12,7 @@ function reverseString(str) {
   //////////////////////////
 
   // let revString = '';
-  // for(let i = str.length - 1; i >= 0; i--) {
+  // for(let i = str.length; i > 0; i--) {
   //   revString = revString + str[i];
   // }
   // return revString;
