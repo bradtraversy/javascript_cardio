1846c957562fffefaa41b7158c65ed091a61f1f1
m0nm <88052316+m0nm@users.noreply.github.com>
2022-07-30 13:58:20 +0100

    Alternative solution to letterChanges

--- session2/index_extra.js
+++ session2/index_extra.js
@@ -165,6 +165,25 @@ function letterChanges(str) {
   // return str.toLowerCase().replace(/[a-z]/g, char => (
   //   char == 'z' ? 'A' : String.fromCharCode(char.charCodeAt() + 1)
   // )).replace(/[aeiou]/g, vowel => vowel.toUpperCase());
+    
+//   SOLUTION BY @m0nm
+//   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('') /* too lazy to split it myself */
+//   const charArr = str.toLowerCase().replace(/[^a-z]/g, "").split('')
+    
+//   charArr.forEach((char, charIndex) => {
+       
+//      if (char === 'z') {
+//          charArr[charIndex] = "a"
+//      }
+      
+//      else {
+//        const index = alphabet.findIndex(alp => char === alp)
+        
+//        charArr[charIndex] = alphabet[index + 1]
+//      }
+//     })
+    
+//     return charArr.join('')
 }
 
 // Call Function
